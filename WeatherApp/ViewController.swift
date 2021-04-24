0a0110feef23811b8aa45e13f045964f047c8d71
Andrei Rauh <rauhandrei69@gmail.com>
2021-04-24 12:43:13 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Rauh Andrei on 24.04.2021.
+//  Copyright © 2021 Rauh Andrei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
